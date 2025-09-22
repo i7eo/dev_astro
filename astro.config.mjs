93f918aeadda8c9bdbc947ec35eea9748dd12cad
i7eo <admin@i7eo.com>
2025-09-22 16:37:30 +0800

    feat: blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chipper-cobbler-26f941.netlify.app',
+  prefetch: true,
+
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  integrations: [vue({jsx: true})],
+
+  experimental: {
+    clientPrerender: true,
+    preserveScriptOrder: true
   },
 });
